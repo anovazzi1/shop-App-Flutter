7b4b241e34514d448547b75d6e2c334ac2c37b2a
otavioanovazzi <otavio2204@gmail.com>
2022-05-04 16:44:58 -0300

    dummy product list added for modeling

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id, title, description, imageUrl;
+  final double price;
+  bool ?isFavorite;
+
+  Product(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.imageUrl,
+      required this.price}
+      );
+}
