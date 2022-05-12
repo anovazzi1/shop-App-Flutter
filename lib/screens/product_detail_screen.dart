c025ba6a2ce759efdb60621c736e2555a3bdaa57
otavioanovazzi <otavio2204@gmail.com>
2022-05-11 22:16:40 -0300

    product detail screen implemented

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -5,14 +5,46 @@ import '../providers/products_provider.dart';
 class ProductDetailScreen extends StatelessWidget {
   static const String productDetailRoute = "/productDetailScreen";
   final String id;
-  const ProductDetailScreen(this.id,{Key? key}) : super(key: key);
+  const ProductDetailScreen(this.id, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final LoadedProduct = Provider.of<Products>(context,listen: false).findById(id);
+    final LoadedProduct =
+        Provider.of<Products>(context, listen: false).findById(id);
     return Scaffold(
       appBar: AppBar(
         title: Text(LoadedProduct.title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                child: Image.network(
+                  LoadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                LoadedProduct.title,
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text('\$${LoadedProduct.price}'),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                LoadedProduct.description,
+                softWrap: true,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
